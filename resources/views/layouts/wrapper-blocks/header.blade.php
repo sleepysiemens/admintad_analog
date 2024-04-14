8f9a70f50ae74d888a00b9700ac9b0a25dadb8e7
sleepysiemens <2001sema@gmail.com>
2024-04-14 20:20:52 +0700

    ui

--- resources/views/layouts/wrapper-blocks/header.blade.php
+++ resources/views/layouts/wrapper-blocks/header.blade.php
@@ -5,11 +5,28 @@
 
         <div>
             <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-                <li>
+                <li class="px-2">
                     <div class="card bg-transparent border-secondary h-100 position-relative">
-                        <i class="fas fa-user-circle fa-2x position-absolute text-white" style="top:0; bottom:0"></i>
-                        <div class="card-body">
-                            <p class="m-0 text-white">{{auth()->user()->name}}</p>
+                        <div class="card-body py-1">
+                            <div class="row">
+                                <div class="col-3 d-flex">
+                                    <i class="fas fa-wallet text-white fa-2x m-auto"></i>
+                                </div>
+                                <div class="col-9">
+                                    <p class="m-0 text-white text-capitalize">{{__('Баланс')}}</p>
+                                    <p class="m-0 text-light">(ID: {{auth()->user()->id}})</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="px-2">
+                    <div class="card bg-transparent border-secondary h-100 position-relative">
+                        <div class="position-absolute d-flex" style="top:0; bottom:0; right: 85%">
+                            <i class="fas fa-user-circle fa-3x text-white my-auto rounded-circle" style="background-color: #3c3e8f !important"></i>
+                        </div>
+                        <div class="card-body px-5 py-1">
+                            <p class="m-0 text-white text-capitalize">{{auth()->user()->name}}</p>
                             <p class="m-0 text-light">(ID: {{auth()->user()->id}})</p>
                         </div>
                     </div>
